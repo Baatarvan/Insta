18bc567c77149a7ff205c171091c36c1cee9b1ed
baatar04 <baatar04@gmail.com>
2021-05-08 23:29:24 +0800

    zasvar

--- src/components/post.js
+++ src/components/post.js
@@ -7,48 +7,69 @@ import { ReactComponent as Chat } from '../assets/chat.svg';
 import { ReactComponent as Mark } from '../assets/Mark.svg';
 import { ReactComponent as Comment } from '../assets/comment.svg';
 
-const Post = (props) => {
+const posts = [
+  {
+    name: 'Max Mustafa',
+    avatar: 'https://i.pravatar.cc/150?img=11',
+    image: 'https://www.w3schools.com/css/img_5terre.jpg',
+    comment: 'Blanka Hickman',
+  },
+  {
+    name: 'Roan Parry',
+    avatar: 'https://i.pravatar.cc/150?img=12',
+    image: 'https://www.w3schools.com/css/lights600x400.jpg',
+    comment: 'Aqib Coleman',
+  },
+];
+
+const Post = () => {
   return (
     <div>
-      <div class='card'>
-        <div class='row'>
-          <div class='col s2 m1 l1 avatarContainer'>
-            <img className='avatar' class='circle' src={props.avatar}></img>
+      {posts.map((item) => {
+        return (
+          <div>
+            <div class='card'>
+              <div class='row'>
+                <div class='col s2 m1 l1 avatarContainer'>
+                  <img
+                    className='avatar'
+                    class='circle'
+                    src={item.avatar}
+                  ></img>
+                </div>
+                <div class='col s8 m10 l10'>
+                  <span class='black-text'>
+                    <h6>{item.name}</h6>
+                  </span>
+                </div>
+                <div class='col s2 m1 l1 right'>
+                  <h4>...</h4>
+                </div>
+              </div>
+              <div class='card-image'>
+                <img className='postImg' class='activator' src={item.image} />
+              </div>
+            </div>
+            <div class='card-content'>
+              <div className='icons'>
+                <Heart />
+                <Chat />
+                <Comment />
+                <span className='right'>
+                  <Mark />
+                </span>
+              </div>
+              <div className='flex'>
+                <p>Liked by</p>
+                <h6>{item.comment}</h6>
+                <p>and</p>
+                <h6>others</h6>
+              </div>
+              <p className='postTime'>18 hours ago</p>
+            </div>
           </div>
-          <div class='col s8 m10 l10'>
-            <span class='black-text'>
-              <h6>{props.name}</h6>
-            </span>
-          </div>
-          <div class='col s2 m1 l1 right'>
-            <h4>...</h4>
-          </div>
-        </div>
-        <div class='card-image'>
-          <img
-            className='postImg'
-            class='activator'
-            src='https://www.w3schools.com/css/img_5terre.jpg'
-          />
-        </div>
-      </div>
-      <div class='card-content'>
-        <div className='icons'>
-          <Heart />
-          <Chat />
-          <Comment />
-          <span className='right'>
-            <Mark />
-          </span>
-        </div>
-        <div className='flex'>
-          <p>Liked by</p>
-          <h6>Bulgantamir</h6>
-          <p>and</p>
-          <h6>others</h6>
-        </div>
-        <p className='postTime'>18 hours ago</p>
-      </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,9 +6,9 @@ const sidebar = (props) => {
   return (
     <div className='sidebar hide-on-med-and-down'>
       <div className='accountSwitch'>
-        <img className='circle' src='https://i.pravatar.cc/150?img=11'></img>
+        <img className='circle' src='https://i.pravatar.cc/150?img=15'></img>
         <div className='account'>
-          <h6 className='username'>Baatarvan</h6>
+          <h6 className='username'>baatar04</h6>
           <p className='firstname'>Baatarvan</p>
         </div>
         <p>Switch</p>
